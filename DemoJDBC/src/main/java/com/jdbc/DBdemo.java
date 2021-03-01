b8de230a8ba34d0fc4e9611719a0ffe3c8820792
Bhushan Patil <bhupatil0001@gmail.com>
2021-03-01 13:51:44 +0530

    Added: jdcb connectivity demo  with database

---
+++ DemoJDBC/src/main/java/com/jdbc/DBdemo.java
@@ -0,0 +1,38 @@
+package com.jdbc;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class DBdemo {
+	public static void main(String[] args) {
+
+		String jdbcURL = "jdbc:mysql://localhost:3307/payroll_service?useSSL=false";
+		String userName = "root";
+		String password = "root";
+		Connection connection;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver loaded!");
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException("Cannot find the driver in the classpath");
+		}
+		listDrivers();
+		try {
+			System.out.println("connecting to the database:" + jdbcURL);
+			connection = DriverManager.getConnection(jdbcURL, userName, password);
+			System.out.println("connection is successful!!!!" + connection);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while (driverList.hasMoreElements()) {
+			Driver driverClass = driverList.nextElement();
+			System.out.println(driverClass.getClass().getName());
+		}
+	}
+}
